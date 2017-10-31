a7c05221fbc55dfca2ef500c5d76f5853c17df38
Yevhen_Kokhan <kohanevgeniy@gmail.com>
2017-10-31 19:23:39 +0200

    removed unused module import

--- src/datetimepicker.module.ts
+++ src/datetimepicker.module.ts
@@ -1,11 +1,7 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {DateTimePickerDirective} from './datetimepicker.directive';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-    ],
     declarations: [
         DateTimePickerDirective
     ],
