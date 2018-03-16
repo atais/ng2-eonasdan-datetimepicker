60e455fafead35eeb02130d940793b507b97c506
Fetrarijaona R <fetrarij@gmail.com>
2018-03-16 16:35:02 +0300

    add examples for webpack

---
+++ examples/webpack/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { 
+    dateTo: any = null;
+    dateModel: any = null;
+    dateToChange(e: any) {
+      this.dateModel = e.format('MM/DD/YYYY hh:mm A')
+    }
+}
